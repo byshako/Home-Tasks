210698b644309e3d787d53121030649412acb877
byshako <byshako@gmail.com>
2017-12-01 18:02:48 +0400

    task-1 Bugs Fix

--- src/app/task-1/task-1.component.ts
+++ src/app/task-1/task-1.component.ts
@@ -9,8 +9,8 @@ export class Task1Component implements OnInit {
     name: string = '';
     editableItem: any;
     toDolist = [
-        {done: true, newName: 'Test 1'},
-        {done: true, newName: 'Test 2'},
+        {done: false, newName: 'Test 1'},
+        {done: false, newName: 'Test 2'},
         {done: false, newName: 'Test 3'},
         {done: false, newName: 'Test 4'},
         {done: false, newName: 'Test 5'},
