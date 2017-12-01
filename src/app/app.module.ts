21dba2bd479dcad5ff8a159a15dc39c47453c81b
byshako <byshako@gmail.com>
2017-12-01 16:20:22 +0400

    Delete Function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Task1Component } from './task-1/task-1.component';
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
     Task1Component,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/task-1/task-1.component.ts
+++ src/app/task-1/task-1.component.ts
@@ -1,22 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-task-1',
-  templateUrl: './task-1.component.html',
-  styleUrls: ['./task-1.component.css']
+    selector: 'app-task-1',
+    templateUrl: './task-1.component.html',
+    styleUrls: ['./task-1.component.css']
 })
 export class Task1Component implements OnInit {
-  name: string = 'tst';
-  toDolist = ['test 1', 'test 2', 'test 3'];
+    name: string;
+    toDolist = [
+        {done: true, newName: 'Test 1'},
+        {done: true, newName: 'Test 2'},
+        {done: false, newName: 'Test 3'},
+        {done: false, newName: 'Test 4'},
+        {done: false, newName: 'Test 5'},
+    ];
+    newName = '';
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
-  onAdd(){
-    this.toDolist.push(this.name);
-  }
-  onRemove(){
-    
-  }
+    ngOnInit() {
+    }
+
+    onAdd() {
+        this.toDolist.push({
+            done: true,
+            newName: this.name
+        });
+        this.name = '';
+    }
+    onDelete(i) {
+        this.toDolist.splice(i, 1);
+    }
+    onDone(i) {
+        console.log('tst');
+    }
+    onEdit() {
+
+    }
 }
