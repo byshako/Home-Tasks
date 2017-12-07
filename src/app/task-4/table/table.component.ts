f68ebf4b5b7cff6385e50c5534d944f8d7ddcfc3
byshako <byshako@gmail.com>
2017-12-07 16:49:44 +0400

    Table Data Service

--- src/app/task-4/table/table.component.ts
+++ src/app/task-4/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TableDataService} from "../table-data.service";
 
 @Component({
   selector: 'app-table',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-
-  constructor() { }
+  myData: any[];
+  constructor(private usersData: TableDataService) { }
 
   ngOnInit() {
+    this.myData = this.usersData.tableData;
   }
 
 }
